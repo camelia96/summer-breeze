99c8084c31918c1e8d99f8d9fd40e3a395e4edf6
camelia <camelia.96str@gmail.com>
2025-12-28 18:18:16 +0100

    design ui components

---
+++ components/design/decorative-divider.tsx
@@ -0,0 +1,15 @@
+export default function DecorativeDivider() {
+    return (
+        <div className="w-full h-fit p-6 items-center  bg-light-green flex justify-between">
+            <h5>Timeless Design</h5>
+            <h5 className="italic font-serif">/</h5>
+            <h5>Inspired by Nature</h5>
+            <h5 className="italic font-serif">/</h5>
+            <h5>Natural Harmony</h5>
+            <h5 className="italic font-serif">/</h5>
+            <h5>Guided by Craft</h5>
+            <h5 className="italic font-serif">/</h5>
+            <h5>Sustainable Design</h5>
+        </div>
+    )
+}
---
+++ components/design/logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+    return (
+        <p className="text-3xl text-dark-green tracking-[-7.5%]">
+            <span className="font-bold text-[30px]">summer</span>
+            <span className="font-serif italic text-[34.5px]">/breeze</span>
+        </p>
+    )
+}
