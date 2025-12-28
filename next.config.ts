49fbf3ba6007f953e784ceba7a540bf3f58ae0a9
camelia <camelia.96str@gmail.com>
2025-12-28 18:19:15 +0100

    images urls nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fastly.picsum.photos',
+        port: '',
+        pathname: '/id/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/seed/picsum',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
