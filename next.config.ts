cc24ab926f5adaa96b779ab9a9fbdf6639571c44
camelia <camelia.96str@gmail.com>
2025-12-31 14:01:21 +0100

    next proper config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,4 @@
 import type { NextConfig } from "next";
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-module.exports = withBundleAnalyzer({})
 const nextConfig: NextConfig = {
   transpilePackages: [],
   compiler: {
