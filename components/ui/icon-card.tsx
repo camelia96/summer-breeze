84d4c6c90c7e3868c5bcf91c220f5716c26f2dbf
camelia <camelia.96str@gmail.com>
2025-12-31 10:39:26 +0100

    dynamic icons + render on component + responsive css

--- components/ui/icon-card.tsx
+++ components/ui/icon-card.tsx
@@ -1,28 +1,34 @@
 import {
     Card,
     CardContent,
-    CardDescription,
-    CardFooter,
     CardHeader,
-    CardTitle,
 } from "@/components/ui/card"
-import { Leaf } from "lucide-react"
-import { ReactNode } from "react"
+import { Fence, Flower, PencilRuler } from "lucide-react";
 
 interface IconCardProps {
-    icon: ReactNode,
+    icon: string,
     title: string,
     description: string,
 }
-export default function IconCard({icon, title, description}: IconCardProps) {
+export default function IconCard({ icon, title, description }: IconCardProps) {
+
+    // Service icons - responsive and dynamic
+    const iconCardStyle = `hidden md:visible text-light-green`;
+    const iconCardStyleResponsive = `visible md:hidden text-light-green`;
+
+    const iconMap = {
+        "Fence": [<Fence key={1} strokeWidth={1} size={60} className={iconCardStyle} />, <Fence key={2} strokeWidth={1} size={40} className={iconCardStyleResponsive} />],
+        "Flower": [<Flower key={1} strokeWidth={1} size={60} className={iconCardStyle} />, <Flower key={2} strokeWidth={1} size={40} className={iconCardStyleResponsive} />],
+        "Pencil Ruler": [<PencilRuler key={1} strokeWidth={1} size={60} className={iconCardStyle} />, <PencilRuler key={2} strokeWidth={1} size={40} className={iconCardStyleResponsive} />],
+    }
     return (
-        <Card className="w-3/12">
-            {icon}
+        <Card className="md:w-3/12">
+            {iconMap[icon as keyof typeof iconMap]}
             <CardHeader>
-                <h3 className="uppercase text-4xl">{title}</h3>
+                <h3 className="uppercase text-2xl md:text-4xl">{title}</h3>
             </CardHeader>
             <CardContent>
-                <p>{description}</p>
+                <p className="text-center md:text-start">{description}</p>
             </CardContent>
         </Card>
     )
