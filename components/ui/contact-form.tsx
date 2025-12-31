5cc438144cc0de97f4fee4c631e49b002259b665
camelia <camelia.96str@gmail.com>
2025-12-31 10:38:50 +0100

    form component create

---
+++ components/ui/contact-form.tsx
@@ -0,0 +1,40 @@
+"use client"
+import { Field, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "./button";
+import { toast } from "sonner";
+export default function ContactForm() {
+    return (
+        <FieldSet>
+            <FieldGroup className="w-11/12">
+                <Field>
+                    <FieldLabel>Name</FieldLabel>
+                    <Input placeholder="Enter your name" />
+                </Field>
+
+
+                <Field>
+                    <FieldLabel>Surname</FieldLabel>
+                    <Input placeholder="Enter your surname" />
+                </Field>
+
+
+                <Field>
+                    <FieldLabel>Email</FieldLabel>
+                    <Input placeholder="Enter your email" />
+                </Field>
+
+
+                <Field>
+                    <FieldLabel>Message</FieldLabel>
+                    <Textarea placeholder="Enter your message" />
+                </Field>
+
+                <Field className="text-center md:text-start">
+                    <Button type="submit" onClick={() => toast.success("Success! You successfully pressed the Submit button")}>Submit</Button>
+                </Field>
+            </FieldGroup>
+        </FieldSet>
+    )
+}
