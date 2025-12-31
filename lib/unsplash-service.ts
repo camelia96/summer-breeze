b9510b7544f29fc2470c5ebc299af7ebc3237074
camelia <camelia.96str@gmail.com>
2025-12-31 13:20:47 +0100

    optim

--- lib/unsplash-service.ts
+++ lib/unsplash-service.ts
@@ -1,5 +1,7 @@
 import { GardenImage } from "./types";
 
+const imageRedimension = "&fm=webp&fit=crop&auto=format&w=";
+
 export async function getGardenPortfolioImages(
   total: number,
   query: string
@@ -7,7 +9,7 @@ export async function getGardenPortfolioImages(
   try {
     const response = await fetch(
       process.env.URL_UNSPLASH! +
-        `search/photos?page=1&per_page=${total}&query=${query}&count=10&orientation=landscape&w=1200&q=80&fit=crop&crop=entropy&auto=format`,
+        `search/photos?page=1&per_page=${total}&query=${query}&count=10&orientation=landscape`,
       {
         headers: {
           Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY!}`,
@@ -16,17 +18,17 @@ export async function getGardenPortfolioImages(
     );
 
     const data = await response.json();
-console.log(data)
+
     const images: GardenImage[] = data.results.map((r: any) => {
       return {
         id: r.id,
         width: r.width,
         height: r.height,
         urls: {
-          full: r.urls.full+"&fm=webp&fit=crop&auto=format&w=1800",
-          regular: r.urls.regular+"&fm=webp&fit=crop&auto=format&w=700",
-          small: r.urls.small+"&fm=webp&fit=crop&auto=format&w=400",
-          raw: r.urls.raw+"&fm=webp&fit=crop&auto=format&w=1800",
+          full: r.urls.full + imageRedimension + "1500",
+          regular: r.urls.regular + imageRedimension + "700",
+          small: r.urls.small + imageRedimension + "400",
+          raw: r.urls.raw + imageRedimension + "1500",
         },
         alt_description: r.alt_description,
       };
@@ -51,10 +53,10 @@ export async function getImageById(id: string): Promise<GardenImage> {
     return {
       id: data.id,
       urls: {
-        full: data.urls.full+"&fm=webp&fit=crop&auto=format&w=1800",
-        regular: data.urls.regular+"&fm=webp&fit=crop&auto=format&w=700",
-        small: data.urls.small+"&fm=webp&fit=crop&auto=format&w=400",
-        raw: data.urls.raw+"&fm=webp&fit=crop&auto=format&w=1800"
+        full: data.urls.full + imageRedimension + "1500",
+        regular: data.urls.regular + imageRedimension + "700",
+        small: data.urls.small + imageRedimension + "400",
+        raw: data.urls.raw + imageRedimension + "1500",
       },
       alt_description: data.alt_description,
       width: data.width,
